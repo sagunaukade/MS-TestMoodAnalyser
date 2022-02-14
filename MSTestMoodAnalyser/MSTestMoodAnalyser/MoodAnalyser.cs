14afd3864619bed3797020533745d2074c2125f0
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-14 23:36:23 +0530

    Update MoodAnalyser.cs

--- MSTestMoodAnalyser/MSTestMoodAnalyser/MoodAnalyser.cs
+++ MSTestMoodAnalyser/MSTestMoodAnalyser/MoodAnalyser.cs
@@ -8,5 +8,35 @@ namespace MSTestMoodAnalyser
 {
     internal class MoodAnalyser
     {
+        
+    private string message;
+
+        /// <summary>
+        /// Parameterised Constructor.
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch
+            {
+                return "HAPPY";
+            }
+        }
     }
 }
