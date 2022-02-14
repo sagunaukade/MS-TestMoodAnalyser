1f498bb0f72461bb7c6a8db163dcd1dd3bd16c25
sagunaukade <sagunaukade96@gmail.com>
2022-02-14 23:33:27 +0530

    [Saguna] Added to given empty mood should throw MoodAnalysisException(TC 3.2)

--- MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalysisException.cs
+++ MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalysisException.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MoodAnalyserTest;
 
 namespace MoodAnalyserTest
 {
-    internal class MoodAnalysisException
-    {
-    }
+     /// <summary>
+        /// MoodAnalysisException Class For Handling Exception.
+        /// </summary>
+        public class MoodAnalysisException : Exception
+        {
+            /// <summary>
+            /// Enum for Exception Type.
+            /// </summary>
+            public enum ExceptionType
+            {
+                NULL_MESSAGE, Empty_Message
+            }
+
+            ////Creating 'type' variable of type ExceptionType
+            private readonly ExceptionType type;
+
+            /// <summary>
+            /// Parameterized Constructor sets the Exception Type and message.
+            /// </summary>
+            /// <param name="Type"></param>
+            /// <param name="message"></param>
+            public MoodAnalysisException(ExceptionType Type, string message) : base(message)
+            {
+                this.type = Type;
+            }
+        }
 }
