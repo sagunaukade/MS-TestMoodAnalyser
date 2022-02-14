22670bc4ba9d7bc40577cd747e04ae053429d04d
sagunaukade <sagunaukade96@gmail.com>
2022-02-14 23:12:42 +0530

    [Saguna] Added to Handle Exception if

--- MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalyserTests.cs
+++ MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalyserTests.cs
@@ -6,42 +6,41 @@ namespace MoodAnalyserTest
     [TestClass]
     public class MoodAnalyserTest
     {
-        
-            /// <summary>
-            /// TC 1.1: Given “I am in Sad Mood” message Should Return SAD.
-            /// </summary>
-            [TestMethod]
-            public void GivenSadMoodShouldReturnSAD()
-            {
-                // Arrange
-                string expected = "SAD";
-                string message = "I am in Sad Mood";
-                moodAnalyser moodAnalyse = new MoodAnalyser(message);
+        /// <summary>
+        /// TC 1.1: Given “I am in Sad Mood” message Should Return SAD.
+        /// </summary>
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            // Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
 
-                // Act
-                string mood = moodAnalyse.AnalyseMood();
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
 
-                // Assert
-                Assert.AreEqual(expected, mood);
-            }
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+        /// <summary>
+        /// TC 1.2  & 2.1: Given “I am in HAPPY Mood” and null message Should Return HAPPY
+        /// </summary>
+        [TestMethod]
+        [DataRow("I am in HAPPY Mood")]
+        [DataRow(null)]
+        public void GivenHAPPYMoodShouldReturnHappy(string message)
+        {
+            // Arrange
+            string expected = "HAPPY";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
 
-            /// <summary>
-            /// TC 1.2: Given “I am in Any Mood” message Should Return HAPPY
-            /// </summary>
-            [TestMethod]
-            public void GivenHAPPYMoodShouldReturnHappy()
-            {
-                // Arrange
-                string expected = "HAPPY";
-                string message = "I am in HAPPY Mood";
-                MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
 
-                // Act
-                string mood = moodAnalyse.AnalyseMood();
-
-                // Assert
-                Assert.AreEqual(expected, mood);
-            }
+            // Assert
+            Assert.AreEqual(expected, mood);
         }
     }
 }
+
