53b9ebc3b430b7a14db570afac2561d0614b7212
sagunaukade <sagunaukade96@gmail.com>
2022-02-14 23:56:27 +0530

    [Saguna] Added to use reflection to create mood analyser with default constructor

---
+++ MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalyserFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+using MoodAnalyserTest;
+
+namespace MoodAnalyzerApp
+{
+    public class MoodAnalyserFactory
+    {
+        /// <summary>
+        /// CreateMoodAnalyse method to create object of MoodAnalyse class.
+        /// </summary>
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnaylseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnaylseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        }
+    }
+}
--- MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalyserTests.cs
+++ MSTestMoodAnalyser/MoodAnalyserTest/MoodAnalyserTests.cs
@@ -7,41 +7,125 @@ namespace MoodAnalyserTest
     [TestClass]
     public class MoodAnalyserTest
     {
-        private string message;
+        /// <summary>
+        /// TC 1.1: Given “I am in Sad Mood” message Should Return SAD.
+        /// </summary>
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            // Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
 
         /// <summary>
-        /// Parameterised Constructor.
+        /// TC 1.2  & 2.1: Given “I am in HAPPY Mood” and null message Should Return HAPPY
         /// </summary>
-        /// <param name="message"></param>
-       // public MoodAnalyser(string message)
-        //{
-         //   this.message = message;
-        //}
+        [TestMethod]
+        [DataRow("I am in HAPPY Mood")]
+        public void GivenHAPPYMoodShouldReturnHappy(string message)
+        {
+            // Arrange
+            string expected = "HAPPY";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
 
-        public string AnalyseMood()
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+
+        /// <summary>
+        /// TC 3.1: Given NULL Mood Should Throw MoodAnalysisException.
+        /// </summary>
+        [TestMethod]
+        public void Given_NULL_Mood_Should_Throw_MoodAnalysisException()
         {
             try
             {
-                if (this.message.Equals(string.Empty))
-                {
-                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.Empty_Message, "Mood should not be Empty");
-                }
-
-
-                if (this.message.Contains("Sad"))
-                {
-                    return "SAD";
-                }
-                else
-                {
-                    return "HAPPY";
-                }
+                string message = null;
+                MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+                string mood = moodAnalyse.AnalyseMood();
             }
-            catch (NullReferenceException)
+            catch (MoodAnalysisException e)
             {
-                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+                Assert.AreEqual("Mood should not be null", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// TC 3.2: Given Empty Mood Should Throw MoodAnalysisException Indicating Empty Mood.
+        /// </summary>
+        [TestMethod]
+        public void Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indicating_EmptyMood()
+        {
+            try
+            {
+                string message = "";
+                MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+                string mood = moodAnalyse.AnalyseMood();
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Mood should not be Empty", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Test Case 4.1 Given MoodAnalyse Class Name Should Return MoodAnalyser Object.
+        /// </summary>
+        [TestMethod]
+        public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+        {
+            object expected = new MoodAnalyser();
+            object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyzerApp.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+
+        /// <summary>
+        /// Test Case 4.2 Given Improper Class Name Should throw MoodAnalyssiException.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperClassNameShouldThrowMoodAnalysisException()
+        {
+            string expected = "Class Not Found";
+            try
+            {
+                object moodAnalyseObject = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyzerApp.DemoClass", "DemoClass");
+
+            }
+            catch (MoodAnalysisException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
+            }
+        }
+
+        // <summary>
+        /// Test Case 4.3 Given Improper Constructor should throw MoodAnalysisException.
+        /// </summary>
+        [TestMethod]
+        public void GivenImproperConstructorShouldThrowMoodAnalysisException()
+        {
+
+            string expected = "Constructor is Not Found";
+            try
+            {
+                object moodAnalyseObject = MoodAnalyserFactory.CreateMoodAnalyser("DemoClass", "MoodAnalyser");
+            }
+            catch (MoodAnalysisException exception)
+            {
+                Assert.AreEqual(expected, exception.Message);
             }
         }
     }
 }
+
 
